350ebd8d6435c532f07e86756b9e0294087379d0
Grace kungu <grayckungu2@gmail.com>
2023-06-20 21:52:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+
+//Lines to remove the 'main' element with id 'main'
+const main = document.querySelector('main');
+main.remove();
+
+// Lines to create an <h1> element with id 'victory' and add it to the DOM
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+document.body.appendChild(newHeader);
